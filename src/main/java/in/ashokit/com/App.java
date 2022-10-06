6a1f00b7aea1840c1dc92ac779792c639fccbd9a
vinodjupalli <vinodhari7jupalli@gmail.com>
2022-10-06 00:02:21 -0400

    new commit

---
+++ src/main/java/in/ashokit/com/App.java
@@ -0,0 +1,13 @@
+package in.ashokit.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
